1423027cd05b7908fbab362535725ce9dd6ebd2f
Thiago Costa <thiagoalvescosta@gmail.com>
2018-05-17 15:48:16 -0300

    Correções diversas

--- src/main/java/cronapi/odata/server/ODataServletV4.java
+++ src/main/java/cronapi/odata/server/ODataServletV4.java
@@ -1,32 +1,16 @@
 package cronapi.odata.server;
 
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import cronapi.QueryManager;
 import org.apache.olingo.commons.api.ex.ODataException;
-import org.apache.olingo.jpa.metadata.api.JPAEdmExtension;
-import org.apache.olingo.jpa.metadata.core.edm.mapper.impl.IntermediateEntitySet;
-import org.apache.olingo.jpa.metadata.core.edm.mapper.impl.IntermediateEntityType;
 import org.apache.olingo.jpa.processor.core.api.JPAODataGetHandler;
 import org.apache.olingo.jpa.processor.core.database.JPADefaultDatabaseProcessor;
 import org.apache.olingo.jpa.processor.core.mapping.JPAPersistenceAdapter;
 import org.apache.olingo.jpa.processor.core.mapping.ResourceLocalPersistenceAdapter;
-import org.apache.olingo.server.api.processor.Processor;
-import org.eclipse.persistence.internal.jpa.parsing.jpql.JPQLParser;
-import org.eclipse.persistence.jpa.JpaQuery;
 
-import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
-import javax.persistence.Tuple;
-import javax.persistence.TypedQuery;
-import javax.persistence.criteria.CriteriaQuery;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.Collection;
-import java.util.List;
-import java.util.Map;
 
 
 public class ODataServletV4 extends HttpServlet {
@@ -59,7 +43,8 @@ public class ODataServletV4 extends HttpServlet {
     final JPAPersistenceAdapter mappingAdapter = new ResourceLocalPersistenceAdapter(namespace,
         entityManagerFactory,
         new JPADefaultDatabaseProcessor());
-    final JPAODataGetHandler handler = new JPAODataGetHandler(mappingAdapter) {
+    final JPAODataGetHandler handler = new JPAODataGetHandler(mappingAdapter);
+ /*   final JPAODataGetHandler handler = new JPAODataGetHandler(namespace) {
       @Override
       protected Collection<Processor> collectProcessors(final HttpServletRequest request,
                                                         final HttpServletResponse response, final EntityManager em) {
@@ -69,7 +54,7 @@ public class ODataServletV4 extends HttpServlet {
       }
     };
 
-    handler.setJPAEdmExtension(new JPAEdmExtension() {
+   /* handler.setJPAEdmExtension(new JPAEdmExtension() {
       @Override
       public void extendJPAEntitySet(Map<String, IntermediateEntitySet> set) {
         JsonObject queries = QueryManager.getJSON();
@@ -93,31 +78,14 @@ public class ODataServletV4 extends HttpServlet {
       public TypedQuery createQuery(EntityManager em, CriteriaQuery<Tuple> criteria) {
         JPQLParser parser = JPQLParser.buildParserFor("select c from Teste c where c.Id = 'asdsad' ");
         parser.parse();
-
-
-        //parser.getParseTree().getWhereNode()
-
-        //criteria.
-      //  TypedQuery<Tuple> query = em.createQuery(
-
-        //    "select c.id as id, c.id2 as id2 from Teste c", Tuple.class);
-
-       // List<Tuple> results = query.getResultList();
-
-        TypedQuery<Tuple> xy = em.createQuery(criteria);
-
-        String jpql = xy.unwrap(JpaQuery.class).getDatabaseQuery().getJPQLString();
-
-        return em.createQuery(criteria);
-
-        //return super.createQuery(criteria);
+        return super.createQuery(criteria);
       }
-    });
+    });*/
 
     return handler;
   }
 
-  private void createDataSource(Map<String, IntermediateEntitySet> set, String id, String entityNamespace) throws Exception {
+  /*private void createDataSource(Map<String, IntermediateEntitySet> set, String id, String entityNamespace) throws Exception {
 
     IntermediateEntitySet foundES = null;
 
@@ -133,7 +101,7 @@ public class ODataServletV4 extends HttpServlet {
       newSet.setExternalName(id);
       set.put(foundES.getInternalName() + "_" + id, newSet);
     }
-  }
+  }*/
 
 
 }
