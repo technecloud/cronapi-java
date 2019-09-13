e8e0b7cc685d79d73c189d3e07f56d05252bc820
Wesley Rover <wesley.rover@cronapp.io>
2019-09-13 16:33:17 -0300

    :white_check_mark: new case test - Var(01)

--- src/test/java/AllTests.java
+++ src/test/java/AllTests.java
@@ -2,18 +2,27 @@ import br.com.cronapi.DatabaseTest;
 import br.com.cronapi.RestClientTest;
 import br.com.cronapi.VarTest;
 import br.com.cronapi.dateTime.DateTimeTest;
+import br.com.cronapi.email.EmailTest;
 import br.com.cronapi.json.JsonTest;
 import br.com.cronapi.list.ListTest;
+import br.com.cronapi.logic.LogicTest;
+import br.com.cronapi.map.MapTest;
 import br.com.cronapi.math.MathTest;
+import br.com.cronapi.odata.DatasourceExtensionTest;
 import br.com.cronapi.rest.security.CronappSecurityTest;
+import br.com.cronapi.util.SecurityUtilTest;
+import br.com.cronapi.util.UtilTest;
 import br.com.cronapi.xml.XmlTest;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
-        DateTimeTest.class, ListTest.class, CronappSecurityTest.class,
-        DatabaseTest.class, RestClientTest.class, VarTest.class, JsonTest.class,
-        XmlTest.class, MathTest.class})
+        DatabaseTest.class, DateTimeTest.class, EmailTest.class, JsonTest.class,
+        ListTest.class, LogicTest.class, MapTest.class, MathTest.class, //DatasourceExtensionTest.class,
+        CronappSecurityTest.class, UtilTest.class, SecurityUtilTest.class,
+        XmlTest.class, DatabaseTest.class,
+        RestClientTest.class, VarTest.class
+        })
 public class AllTests {
 }
---
+++ src/test/java/br/com/cronapi/VarDeserializerTest.java
@@ -0,0 +1,7 @@
+package br.com.cronapi;
+
+import static org.junit.Assert.*;
+
+public class VarDeserializerTest {
+
+}
