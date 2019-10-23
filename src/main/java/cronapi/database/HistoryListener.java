caff3985ba0a06db05648d1f11e6f468e27be7cb
Thiago Costa <thiagoalvescosta@gmail.com>
2019-10-23 12:03:56 -0300

    Suporte a affected fields e serialização correta de datas

--- src/main/java/cronapi/database/HistoryListener.java
+++ src/main/java/cronapi/database/HistoryListener.java
@@ -3,22 +3,39 @@ package cronapi.database;
 import com.google.gson.*;
 import cronapi.RestClient;
 import cronapi.Utils;
+import cronapi.util.GsonUTCDateAdapter;
 import cronapi.util.ReflectionUtils;
+import org.eclipse.persistence.descriptors.DescriptorEvent;
+import org.eclipse.persistence.descriptors.DescriptorEventAdapter;
+import org.eclipse.persistence.queries.UpdateObjectQuery;
 
-import javax.persistence.*;
-import javax.persistence.metamodel.EntityType;
-import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.Id;
 import java.net.Inet4Address;
 import java.net.InetAddress;
 import java.net.NetworkInterface;
 import java.net.SocketException;
 import java.util.Date;
 import java.util.Enumeration;
-import java.util.Set;
 
-public class HistoryListener {
+public class HistoryListener extends DescriptorEventAdapter {
 
   private static final String CURRENT_IP = getCurrentIp();
+  private GsonUTCDateAdapter UTCDateAdaper = new GsonUTCDateAdapter();
+
+  @Override
+  public void postUpdate(DescriptorEvent event) {
+    beforeAnyOperation(event, "UPDATE");
+  }
+
+  @Override
+  public void postInsert(DescriptorEvent event) {
+    beforeAnyOperation(event, "INSERT");
+  }
+
+  @Override
+  public void postDelete(DescriptorEvent event) {
+    beforeAnyOperation(event, "DELETE");
+  }
 
   private static String getCurrentIp() {
 
@@ -41,21 +58,6 @@ public class HistoryListener {
     return null;
   }
 
-  @PostPersist
-  private void beforeInsert(Object object) {
-    beforeAnyOperation(object, 0);
-  }
-
-  @PostRemove
-  private void beforeUpdate(Object object) {
-    beforeAnyOperation(object, 1);
-  }
-
-  @PostUpdate
-  private void beforeRemove(Object object) {
-    beforeAnyOperation(object, 2);
-  }
-
   private JsonElement toElement(Gson gson, Object value) {
     if (value instanceof Date) {
       return new JsonPrimitive(Utils.getISODateFormat().format((Date) value));
@@ -65,14 +67,15 @@ public class HistoryListener {
     }
   }
 
-  private void beforeAnyOperation(Object object, int operation) {
+  private void beforeAnyOperation(DescriptorEvent event, String operation) {
 
     Class auditClazz = null;
+    Object object = event.getObject();
 
     try {
       auditClazz = Class.forName(object.getClass().getPackage().getName() + ".AuditLog");
 
-      Gson gson = new GsonBuilder().addSerializationExclusionStrategy(new ExclusionStrategy() {
+      GsonBuilder builder = new GsonBuilder().addSerializationExclusionStrategy(new ExclusionStrategy() {
         @Override
         public boolean shouldSkipField(FieldAttributes fieldAttributes) {
           if (fieldAttributes.getDeclaringClass() == object.getClass() || fieldAttributes.getAnnotation(Id.class) != null) {
@@ -85,36 +88,19 @@ public class HistoryListener {
         public boolean shouldSkipClass(Class<?> aClass) {
           return false;
         }
-      }).create();
+      });
 
-      Class domainClass = object.getClass();
-      EntityManager em = TransactionManager.getEntityManager(domainClass);
+      builder.registerTypeAdapter(Date.class, UTCDateAdaper);
+
+      Gson gson = builder.create();
 
       JsonElement objectJson = gson.toJsonTree(object);
 
-      JsonElement keys = new JsonObject();
-
-      EntityType type = em.getMetamodel().entity(domainClass);
-
-      Set attrs = JPAUtil.getAjustedAttributes(type);
-
-      int total = 0;
-      for (Object obj : attrs) {
-        SingularAttribute field = (SingularAttribute) obj;
-        if (field.isId()) {
-          total++;
-        }
-      }
-
-      for (Object obj : attrs) {
-        SingularAttribute field = (SingularAttribute) obj;
-        if (field.isId()) {
-          Object value = ReflectionUtils.getField(object, field.getName());
-          if (total == 1) {
-            keys = toElement(gson, value);
-          } else {
-            keys.getAsJsonObject().add(field.getName(), toElement(gson, value));
-          }
+      JsonArray affected = null;
+      if (event.getQuery() instanceof UpdateObjectQuery) {
+        affected = new JsonArray();
+        for (String field : ((UpdateObjectQuery) event.getQuery()).getObjectChangeSet().getChangedAttributeNames()) {
+          affected.add(field);
         }
       }
 
@@ -124,22 +110,17 @@ public class HistoryListener {
       ReflectionUtils.setField(auditLog, "command", operation);
       ReflectionUtils.setField(auditLog, "date", new Date());
       ReflectionUtils.setField(auditLog, "objectData", objectJson.toString());
-      if (keys.isJsonPrimitive() && keys.getAsJsonPrimitive().isString()) {
-        ReflectionUtils.setField(auditLog, "objectKeys", keys.getAsString());
-      } else {
-        ReflectionUtils.setField(auditLog, "objectKeys", keys.toString());
-      }
       if (RestClient.getRestClient() != null) {
         ReflectionUtils.setField(auditLog, "user", RestClient.getRestClient().getUser() != null ? RestClient.getRestClient().getUser().getUsername() : null);
         ReflectionUtils.setField(auditLog, "host", RestClient.getRestClient().getHost());
         ReflectionUtils.setField(auditLog, "agent", RestClient.getRestClient().getAgent());
       }
       ReflectionUtils.setField(auditLog, "server", CURRENT_IP);
+      ReflectionUtils.setField(auditLog, "affectedFields", affected != null ? affected.toString() : null);
 
-      em.persist(auditLog);
+      event.getSession().insertObject(auditLog);
 
       System.out.println(objectJson.toString());
-      System.out.println(keys.toString());
     } catch (Exception e) {
       e.printStackTrace();
     }
