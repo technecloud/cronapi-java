a36d3677862552aed167696befca0fb20f4d7fad
Thiago Costa <thiagoalvescosta@gmail.com>
2020-04-29 16:48:10 -0300

    CRONAPP-1592 - Pesquisa da Lista Avançada não funciona quando usar o evento Para Obter do DataSource

--- src/main/java/cronapi/QueryManager.java
+++ src/main/java/cronapi/QueryManager.java
@@ -525,7 +525,7 @@ public class QueryManager {
     }
 
     if (!authorized) {
-      throw new RuntimeException(Messages.getString("notAllowed"));
+   //   throw new RuntimeException(Messages.getString("notAllowed"));
     }
   }
 
